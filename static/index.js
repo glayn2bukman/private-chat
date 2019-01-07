119e8ab8d44956d8a05868d2af1b5420a2e3967a
glayn2bukman <glayn2bukman@gmail.com>
2019-01-07 17:42:17 +0300

    Update index.js

--- static/index.js
+++ static/index.js
@@ -1107,6 +1107,6 @@ window.onload = function() {
         calc_btns[i].onclick = calc_input;
     }
 
-    document.addEventListener("keydown", function(e){if(e.keyCode == 27){back();}}, false);
+    document.addEventListener("keydown", function(e){if(e.keyCode == 27){event.preventDefault();back();}}, false);
 
 };
